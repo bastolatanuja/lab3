02516af68b7f9ced8ea840e07b97119523d6e22a
bastolatanuja <tanujabastola143@gmail.com>
2021-04-20 11:53:25 +0545

    python function

--- q1.py
+++ q1.py
@@ -1,7 +1,7 @@
 #write a python function to find the max of three number.
 def max(a,b,c):
     if a>b and a>c:
-        print(f"a is maximum number")
+        print(f" a is maximum number")
     elif b>a and b>c:
         print(f"b is maximum number")
     else:
